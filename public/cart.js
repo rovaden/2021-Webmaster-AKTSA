634b09955d857dace90059e66d9fdb16aa4e291c
alice15311 <57412796+alice15311@users.noreply.github.com>
2021-02-10 08:18:15 -0500

    changed logging to seperate script b/c of ref errors in client console

--- public/cart.js
+++ public/cart.js
@@ -4,9 +4,8 @@ if (document.readyState == 'loading'){
   ready()
 }
 
-
 function ready() {
-    var removeItemButtons = document.getElementsByClassName('delete-btn')
+  var removeItemButtons = document.getElementsByClassName('delete-btn')
 
   for (var i = 0; i < removeItemButtons.length; i++) {
     var button = removeItemButtons[i];
@@ -26,7 +25,6 @@ function ready() {
   }
 
   updateCartTotal();
-  logCart();
 }
 
 function removeCartItemFuncCreator(button){
@@ -123,38 +121,4 @@ function updateCartTotal() {
 
   total = Math.round(total*100)/100
   document.getElementsByClassName('cart-total-price')[0].innerText = "$" + total
-}
-
-function logCart(){
-  var button = document.getElementById('cart-check-out');
-  button.addEventListener('click', function(e) {
-    console.log('button was clicked');
-    var data = {};
-    var cartItemContainer = document.getElementsByClassName('shopping-cart')[0];
-    var cartItems = cartItemContainer.getElementsByClassName('item');
-    console.log(document.getElementsByClassName('cart-total-price')[0].innerHTML);
-    for (var i=0; i < cartItems.length; i++){
-      data["item " + (i+1)] = {
-        itemName: document.getElementsByClassName('cart-item-title')[i].innerText, 
-        itemQuantity: document.getElementsByClassName('cart-quantity-input')[i].value,
-        itemTotalPrice: document.getElementsByClassName('total-price')[i].innerText
-      };
-    }
-    data["totalCartPrice"] = document.getElementsByClassName('cart-total-price')[0].innerHTML;
-    fetch('/cart', {
-      method: 'POST', 
-      headers: {
-        'Content-Type': 'application/json',
-        'Accept': 'application/json'
-      },
-      body: JSON.stringify(data),
-    })
-    .then(response => response.json())
-    .then(data => {
-      console.log('Success:', data);
-    })
-    .catch((error) => {
-      console.error('Error:', error);
-    });
-  });
 }
