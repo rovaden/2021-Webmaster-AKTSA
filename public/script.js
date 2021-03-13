0ed0553d89b8680ea91f12c3d545dfcc2f6c7af6
Catherine Pan <catherine.pan27@gmail.com>
2021-03-12 19:43:17 -0500

    made nav fixed, change styling on scroll

--- public/script.js
+++ public/script.js
@@ -1,31 +1,35 @@
+
+let hasSetUpMobile = false;
+let allowScrollAnim = false;
+const navBar = document.getElementsByTagName("nav")[0];
+
+//dropdown menus toggle function for mobile
+let toggleDropdown = function(liEl, divEl){
+  //getting elements + target height
+  const aEl = liEl.firstChild;
+  const target = divEl;
+  const maxHeight = divEl.scrollHeight;
+  let clicks = 0;
+
+  //event handler function that toggles height
+  return function(){
+    clicks++;
+    aEl.classList.toggle("active");
+    if(clicks % 2 === 0){
+      target.style.maxHeight = "0";
+    }
+    else {
+      target.style.maxHeight = maxHeight + "px";
+    }
+  };
+}
+
 //navigation on mobile
-const navMenu = document.getElementById("navMenu");
-if(navMenu.style.display !== "none"){
-  const navBar = document.getElementsByTagName("nav")[0];
+function setUpMobile(){
+  //toggling active class when clicked
   navMenu.addEventListener("click", function(){
     navBar.classList.toggle("active");
   });
-
-  //dropdown menus toggle function
-  let toggleDropdown = function(liEl, divEl){
-    //getting elements + target height
-    const aEl = liEl.firstChild;
-    const target = divEl;
-    const maxHeight = divEl.scrollHeight;
-    let clicks = 0;
-
-    //event handler function that toggles height
-    return function(){
-      clicks++;
-      aEl.classList.toggle("active");
-      if(clicks % 2 === 0){
-        target.style.maxHeight = "0";
-      }
-      else {
-        target.style.maxHeight = maxHeight + "px";
-      }
-    };
-  }
 
   //looping through dropdown menus in the navigation bar
   const dropDowns = navBar.getElementsByClassName("dropdown");
@@ -34,4 +38,55 @@ if(navMenu.style.display !== "none"){
     liEl = divEl.parentElement;
     liEl.addEventListener("click", toggleDropdown(liEl, divEl));
   }
+
+  //set has set up mobile to true, so it's not needed the next time
+  hasSetUpMobile = true;
 }
+
+//changing navigation style on scroll
+let setUpNavScroll = (function(){
+  const prevClassName = navBar.className;
+  let scrollWait = false;
+  let handleScroll = function(){
+    if(!scrollWait){
+      window.requestAnimationFrame(function(){
+        if(document.body.scrollTop > 20 || document.documentElement.scrollTop > 20){
+          navBar.className = "navbar scrolledDown";
+        }
+        else {
+          navBar.className = prevClassName;
+        }
+        scrollWait = false;
+      });
+      scrollWait = true;
+    }
+  }
+  return function(initial){
+    scrollWait = false;
+    if(allowScrollAnim){
+      if(initial){
+        handleScroll();
+      }
+      window.addEventListener("scroll", handleScroll);
+    }
+    else {
+      window.removeEventListener("scroll", handleScroll);
+    }
+  }
+})();
+
+//handling both setting up mobile navigation and scrolling
+function handleAll(initial){
+  if(getComputedStyle(navMenu).display === "block"){
+    if(!hasSetUpMobile){
+      setUpMobile();
+    }
+    allowScrollAnim = false;
+  }
+  else {
+    allowScrollAnim = true;
+  }
+  setUpNavScroll(initial);
+}
+handleAll(true);
+window.addEventListener("resize", handleAll);
